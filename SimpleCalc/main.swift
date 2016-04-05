9f747375985eacea9538ece1277217c1fde5f980
Alyssa <ahudon@uw.edu>
2016-04-05 13:05:25 -0700

    revised calculator functionality

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -2,8 +2,8 @@
 //  main.swift
 //  SimpleCalc
 //
-//  Created by iGuest on 4/5/16.
-//  Copyright © 2016 iGuest. All rights reserved.
+//  Created by Alyssa on 4/5/16.
+//  Copyright © 2016 Alyssa. All rights reserved.
 //
 
 import Foundation
@@ -12,56 +12,101 @@ var count = 0
 
 var sum = 0
 
-var num1: UInt
-
-var num2: UInt
+var lastNumber: UInt?
 
 var anotherCalc = true
 
 var doOperation = false
 
-var operand: String
+var operand = ""
+
+func resetVariables() {
+    lastNumber = 0;
+    count = 0;
+    sum = 0;
+}
 
 print("Enter an expression separated by returns:")
 
 while anotherCalc {
-    print("going into loop")
     var input = readLine(stripNewline: true)!
     
-    print("here is the input I received \(input)")
-    
     if (input == "exit") {
-        print("exiting after this calculation")
-        anotherCalc = false
+        print("exiting")
+        exit(0)
     }
 
     var num = UInt.init(input)
-    
-    print("here is the value of num \(num)")
 
     if (num != nil) {
-        num = num!
-        count = count++
+        count += 1
         sum += Int(num!)
         
+        if (lastNumber == nil) {
+            lastNumber = num!
+        }
+        
         if (doOperation) {
-            
+            switch operand {
+            case "+" :
+                var result = lastNumber! + num!
+                print("\(result)")
+            case "-":
+                var result = Int(lastNumber!) - Int(num!)
+                print("\(result)")
+            case "%":
+                var result = lastNumber! % num!
+                print("\(result)")
+            case "/":
+                var result = Double(lastNumber!) / Double(num!)
+                print("\(result)")
+            case "*":
+                var result = lastNumber! * num!
+                print("\(result)")
+            default :
+                print("Please enter a positive whole number or an operator.")
+            }
+            doOperation = false
+            resetVariables()
+        } else {
+            lastNumber = num!
         }
         
     } else if input == "+" || input == "-" || input == "%" || input == "/" || input == "*" {
         operand = input
-        print("here is the operand that was saved \(operand)")
+        doOperation = true
     } else if input == "count" {
-        print(count)
+        print("The count of the numbers entered is: \(count)")
+        resetVariables()
     } else if input == "avg" {
-        print(count / sum)
+        if (count == 0) {
+            print("Please enter numbers to compute an average.")
+        } else {
+            var result = Double(sum / count)
+            print("The average of these numbers is: \(result)")
+        }
+        resetVariables()
     } else if input == "fact" {
+        var result = 0
+        if (lastNumber! == 0) {
+            result = 1
+        } else {
+            result = 1
+            for var i = Int(lastNumber!); i > 1; i-- {
+                result *= i
+            }
+        }
         
+        print("The factorial of \(lastNumber!) is: \(result)")
+        resetVariables()
     } else {
-        print("Please enter a positive number whole number or an operator.")
+        print("Please enter a positive whole number or an operator.")
+        resetVariables()
     }
 
 }
+
+
 
 
 
